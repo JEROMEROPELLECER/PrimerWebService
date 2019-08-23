d47011fbf0759cddb48be825ed02c29fcfcca3ce
jeromero <jeromero@CCDPD02>
2019-08-23 16:53:32 -0600

    Consumo de web service

--- SWCANDELARIA/SWCANDELARIA/WSControlHombres.asmx.cs
+++ SWCANDELARIA/SWCANDELARIA/WSControlHombres.asmx.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Data.SqlClient;
+
 
 namespace SWCANDELARIA
 {
@@ -14,10 +17,43 @@ namespace SWCANDELARIA
     [System.ComponentModel.ToolboxItem(false)]
     // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
     // [System.Web.Script.Services.ScriptService]
+
+
     public class WSControlHombres : System.Web.Services.WebService
     {
 
         [WebMethod]
+        public DataSet Getdata()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "@Data Source= 127.0.0.1\\SQL ;Initial Catalog = CANDE;Integrated Security=False;User Id=sa;Password=Dpdlanman2010";
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM ADM_MAESTRO_HOMBRES_2010 " ,conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+
+       /* [WebMethod]
+        public double Square(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+        
+
+
+
+
+        /*[WebMethod]
+        public string Hola(string Nombre)
+        {
+            return "Hola" + Nombre;
+        }
+        /*public string FyH()
+        {
+            return DateTime.Now.ToString();
+        }*/
+
+       /* [WebMethod]
         public string HelloWorld()
         {
             return "Control de candelaria Hombres  ";
@@ -33,7 +69,7 @@ namespace SWCANDELARIA
             catch
             {
                 return false;
-            }
-        }
+            }*/
+        //}
     }
 }
---
+++ SWCANDELARIA/SWCAT/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SWCAT
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WSDB.WSControlHombresSoapClient WS = new WSDB.WSControlHombresSoapClient();
+            DataSet ds = WS.Getdata();
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+        }
+
+
+
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
